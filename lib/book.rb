4cb43d709df21090974ae4cf799e4bedaffc6691
Isaac Thill <ithill22@turing.edu>
2023-02-24 10:03:33 -0700

    fix: removed author from attributes and made new author method

--- lib/book.rb
+++ lib/book.rb
@@ -2,15 +2,17 @@ class Book
   attr_reader :title,
               :author_first_name,
               :author_last_name,
-              :publication_date,
-              :author
+              :publication_date
 
   def initialize(book_details)
     @title = book_details[:title]
     @author_first_name = book_details[:author_first_name]
     @author_last_name = book_details[:author_last_name]
-    @publication_date = book_details[:publication_date]
-    @author = author_first_name + (' ') + author_last_name   
+    @publication_date = book_details[:publication_date]      
+  end
+
+  def author
+    author = author_first_name + (' ') + author_last_name
   end
 
   def publication_year
